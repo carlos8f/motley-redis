c62342a78ccedd657e0b3f0177fa817aafd90da2
Carlos Rodriguez <carlos@s8f.org>
2016-04-28 16:45:40 -0700

    return task func

--- db/createRedisCollection.js
+++ db/createRedisCollection.js
@@ -1,10 +1,12 @@
 module.exports = function container (get, set) {
   return function createRedisCollection (name, options, cb) {
-    var coll = get('vendor.sosa_redis')({
-      client: get('db.redis.client'),
-      prefix: get('conf.db.redis').key_prefix
-    })(name, options);
-    set('db.' + name, coll);
-    setImmediate(cb);
+    return function task (cb) {
+      var coll = get('vendor.sosa_redis')({
+        client: get('db.redis.client'),
+        prefix: get('conf.db.redis').key_prefix
+      })(name, options);
+      set('db.' + name, coll);
+      setImmediate(cb);
+    }
   }
 }
