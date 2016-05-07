d49d4e53ac0e66799c9e445c385186fc8369946c
Carlos Rodriguez <carlos@s8f.org>
2016-05-06 19:57:14 -0700

    use new console

--- db/closeRedis.js
+++ db/closeRedis.js
@@ -2,10 +2,10 @@ module.exports = function container (get, set) {
   return function closeRedis (cb) {
     var client = get('db.redis.client')
     if (client) {
-      get('vendor.console').log('motley-redis: closing connection...')
+      get('console').log('motley-redis: closing connection...')
       client.quit(function (err) {
         if (err) return cb(err)
-        get('vendor.console').log('motley-redis: connection closed.')
+        get('console').log('motley-redis: connection closed.')
         cb()
       })
     }
--- db/connectRedis.js
+++ db/connectRedis.js
@@ -2,12 +2,12 @@ module.exports = function container (get, set) {
   return function connectRedis (cb) {
     var options = get('conf.db.redis');
     if (!options.password) delete options.password;
-    get('vendor.console').log('motley-redis: connecting...')
+    get('console').log('motley-redis: connecting...')
     var client = get('vendor.redis').createClient(options);
     set('@db.redis.client', client);
     client.once('ready', function () {
       delete options.password
-      get('vendor.console').log('motley-redis: connected.', options)
+      get('console').log('motley-redis: connected.', options)
       cb()
     })
   }
